578a9c0c82edfe240f545326187dc615418cf681
creativechern <creativechern@outlook.com>
2021-01-09 18:08:10 +0000

    Added assignment1.py

---
+++ assignment1.py
@@ -0,0 +1,20 @@
+# Problem 1. Lambda function, list, class and sort function.
+# using lambda function to sort a list of objects.  Create a list of actors using the 
+# above Actor class and then sort the list using first name and last name respectively.
+
+# Problem 2. Dictionary usage. 
+# Given an array of integers, return indices of the two numbers such that they add up to a 
+# specific target. You may assume that each input would have exactly one solution, and you 
+# may not use the same element twice.
+# Example: Given nums = 2, 7, 11, 15, target = 9,
+# Because nums0 + nums1 = 2 + 7 = 9, return 0, 1.
+
+# Problem 3. Input function and string operations. 
+# 1) Prompt "What is your first name?" and let user to input the first name
+# 2) Prompt "What is your last name?" and let user to input the last name.
+# 3) Based on 1 and 2, we can get the full name by combing first name and last 
+#   name. Print out "Your full name is " and the full name. 
+
+# asbc
+# sdfsdaf
+# asdfsafas
