92e35fd62ae425dff31729f6dfa0a2dc802822c0
Xuehua Chen <xuehua@gmail.com>
2021-01-16 10:44:21 -0800

    Added solution.ppy to provide solutions to the exercise problems.

---
+++ solution.py
@@ -0,0 +1,19 @@
+def solution1(): 
+    first_name = input("What is your first name?\n")
+    last_name = input("What is your last name?\n")
+    full_name = f"{first_name} {last_name}"
+    print(f"Your full name is {full_name}") 
+
+
+
+
+
+
+
+
+def main():
+    solution1()
+
+
+if __name__ == "__main__":
+    main()
